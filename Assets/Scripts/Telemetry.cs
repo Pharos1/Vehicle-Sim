51bd8ad7d2bb53aee146af61e1fe6261cce1b150
Pharos1 <pharosisdoingnothing@gmail.com>
2025-08-27 11:17:42 +0300

    Fully functional Telemetry and Update to Todo.txt
    Telemetry design is from Edy's Vehicle Physics Pro

--- Assets/Scripts/Telemetry.cs
+++ Assets/Scripts/Telemetry.cs
@@ -1,50 +1,95 @@
 using UnityEngine;
 using UnityEngine.TextCore.Text;
+using System;
 
 public class Telemetry : MonoBehaviour {
-    [SerializeField] private bool showTelemetry = true;
+    private bool showTelemetry = true;
 
-    [SerializeField] private Transform body;
+    [SerializeField] private Transform car;
 
     [SerializeField] private Font font;
     [SerializeField] private FontAsset fontass;
 
     private Rigidbody rb;
-    public bool pointMode;
+    private CarController cc;
+    [SerializeField] private bool pointFiltering; //Gives pixelated look (Due to some bug in Unity's code, forced point filtering for fonts in Unity 6 doesn't work and doesn't give errors)
+
+    [SerializeField] private KeyCode hotkey = KeyCode.F1;
+    [SerializeField] private int fontSize = 10;
+    [SerializeField] private Color textColor = Color.white;
+    [SerializeField] private int spacingCorrection = -10;
 
     private void Start() {
-        rb = body.GetComponent<Rigidbody>();
+        rb = car.GetComponent<Rigidbody>();
+        cc = car.GetComponent<CarController>();
     }
 
-    void OnGUI() {
+    private void Update() {
+        if (Input.GetKeyDown(hotkey)) {
+            showTelemetry = !showTelemetry;
+        }
+    }
+
+    private void OnGUI() {
         if (!showTelemetry)
             return;
 
-        GUIStyle style = new GUIStyle(GUI.skin.label);
-        style.fontSize = 12;
-        style.normal.textColor = Color.white;
-        style.alignment = TextAnchor.UpperLeft;
-        style.font = font; // Monospace look
-
-        if (pointMode) font.material.mainTexture.filterMode = FilterMode.Point;
+        //Filtering
+        if (pointFiltering) font.material.mainTexture.filterMode = FilterMode.Point;
         else font.material.mainTexture.filterMode = FilterMode.Bilinear;
 
-        GUILayout.BeginArea(new Rect(10, 10, 400, 200), GUI.skin.box);
-        GUILayout.Label("Telemetry", style);
+        //Styles
+        GUIStyle style = new GUIStyle(GUI.skin.label);
+        style.font = font;
+        style.fontSize = fontSize;
+        style.normal.textColor = textColor;
+        style.hover.textColor = textColor;
+        style.alignment = TextAnchor.UpperLeft;
+        style.wordWrap = false;
 
-        float speedMS = rb.velocity.magnitude;
+        GUIStyle centeredStyle = GUI.skin.GetStyle("Label");
+        centeredStyle.font = font;
+        centeredStyle.fontSize = fontSize;
+        centeredStyle.normal.textColor = textColor;
+        centeredStyle.hover.textColor = textColor;
+        centeredStyle.alignment = TextAnchor.UpperCenter;
+
+        //GUI Rendering
+        GUILayout.BeginArea(new Rect(10, 10, 500, 120), GUI.skin.box);
+
+        GUILayout.Label("Telemetry" + " | \"" + hotkey.ToString() + "\" To Hide/Show", centeredStyle); GUILayout.Space(-5);
+
+        float speedMS = Mathf.Abs(cc.V.y);
         float speedKMH = speedMS * 3.6f;
         float speedMPH = speedMS * 2.23694f;
 
-        GUILayout.Label($"V: {speedMS:F1} m/s  {speedKMH:F1} km/h  {speedMPH:F1} mph", style);
-        GUILayout.Label($"WheelFl     : 0 rpm", style);
+        GUILayout.Label($"V: {speedMS:F1} m/s  {speedKMH:F1} km/h  {speedMPH:F1} mph", style); GUILayout.Space(-5);
 
-        //ShowWheel("WheelFL", wheelFL, style);
-        //ShowWheel("WheelFR", wheelFR, style);
-        //ShowWheel("WheelRL", wheelRL, style);
-        //ShowWheel("WheelRR", wheelRR, style);
+        showWheelLabel(cc.wheels[0], style); GUILayout.Space(spacingCorrection);
+        showWheelLabel(cc.wheels[1], style); GUILayout.Space(spacingCorrection);
+        showWheelLabel(cc.wheels[2], style); GUILayout.Space(spacingCorrection);
+        showWheelLabel(cc.wheels[3], style); GUILayout.Space(-5);
+
+        GUILayout.Label($"Gear: {cc.curGear}  RPM: {cc.rpm:F0}", style); GUILayout.Space(-5);
+
+        style.alignment = TextAnchor.LowerRight;
+        style.wordWrap = true;
+        style.contentOffset = new Vector2(10, 0);
+        GUILayout.Label("Credits: Design from Edy's VPP");
 
         GUILayout.EndArea();
     }
+    private void showWheelLabel(Wheel wheel, GUIStyle style) {
+        float wheelRpm = (wheel.V.y / wheel.radius) * (60 / (2 * Mathf.PI));
+        float Fs = wheel.s.suspensionForce;
+        Vector2 S = Vector2.zero;
+        Vector2 F = wheel.F;
 
+        if (wheel.grounded) {
+            GUILayout.Label($"Wheel{wheel.type}  : {wheelRpm,4:F0} rpm  Fs: {Fs,3:F0}  Sx: {S.x,2:F2}  Sy: {S.y,2:F2}  Fx: {F.x,6:F2}  Fy: {F.y,6:F2}", style);
+        }
+        else {
+            GUILayout.Label($"Wheel{wheel.type}  : {wheelRpm,4:F0} rpm  --", style);
+        }
+    }
 }
