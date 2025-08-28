23166a2bb18d9960b8584e6f2ce7450c40a1ea24
Pharos1 <pharosisdoingnothing@gmail.com>
2025-08-28 10:10:15 +0300

    Removed useless EndHorizontal() in Editors
    It's hard being stupid😢

--- Assets/Scripts/Editors/Suspension Editor.cs
+++ Assets/Scripts/Editors/Suspension Editor.cs
@@ -14,25 +14,10 @@ public class SuspensionEditor : Editor {
 
 		Suspension.showSuspensionList = EditorGUILayout.Foldout(Suspension.showSuspensionList, "Suspension Config");
 		if (Suspension.showSuspensionList) {
-			//Ck
-			EditorGUILayout.BeginHorizontal();
 			so.FindProperty("Ck").floatValue = EditorGUILayout.Slider("Spring Stiffness", s.Ck, 0, 1);
-			EditorGUILayout.EndHorizontal();
-
-			//Cd
-			EditorGUILayout.BeginHorizontal();
 			so.FindProperty("Cd").floatValue = EditorGUILayout.Slider("Damper Stiffness", s.Cd, 0, 1);
-			EditorGUILayout.EndHorizontal();
-
-			//Rest Length
-			EditorGUILayout.BeginHorizontal();
 			so.FindProperty("restLength").floatValue = EditorGUILayout.FloatField("Rest Length", s.restLength);
-			EditorGUILayout.EndHorizontal();
-
-			//Spring Travel
-			EditorGUILayout.BeginHorizontal();
 			so.FindProperty("springTravel").floatValue = EditorGUILayout.FloatField("Spring Travel", s.springTravel);
-			EditorGUILayout.EndHorizontal();
 		}
 
 		so.ApplyModifiedProperties();
