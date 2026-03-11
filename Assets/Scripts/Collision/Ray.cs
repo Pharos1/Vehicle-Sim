115aeb6a56616c0fe2872b69a34846d01a5285e4
Pharos1 <pharosisdoingnothing@gmail.com>
2026-03-11 20:58:27 +0200

    New Ray and Patch classes

--- Assets/Scripts/Collision/Ray.cs
+++ Assets/Scripts/Collision/Ray.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ray : MonoBehaviour {
-    RaycastHit hit;
-    float lenToOrigin; //Length to origin ray of Patch.cs
-    float lenToRad; //Length to radius
+[System.Serializable]
+public class Ray {
+	public Vector3 origin;
+	public float centerToPointDist;
+
+
+	//Old WIP Ideas
+	public float lenToOrigin; //Length to origin ray of Patch.cs
+	public float lenToRad; //Length to radius
+
+	public Ray(Vector3 origin, float centerToPointDist) {
+		this.origin = origin;
+		this.centerToPointDist = centerToPointDist;
+	}
 }
